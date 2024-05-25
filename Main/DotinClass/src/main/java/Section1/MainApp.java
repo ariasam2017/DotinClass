d216dab0eee75b8e7b6766e7887b6a05ef0a245f
Amirhossein <amir.kingstars@gmail.com>
2024-05-25 10:02:19 +0330

    commit

--- Main/DotinClass/src/main/java/Section1/MainApp.java
+++ Main/DotinClass/src/main/java/Section1/MainApp.java
@@ -2,6 +2,6 @@ package Section1;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("Hello Dotin School");
+        System.out.println("Hello Dotin School")
     }
 }
