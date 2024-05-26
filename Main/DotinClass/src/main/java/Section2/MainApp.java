15f802b1b231e93e724e082972823512891d4cca
Amirhossein <amir.kingstars@gmail.com>
2024-05-26 16:46:04 +0330

    commit

--- Main/DotinClass/src/main/java/Section2/MainApp.java
+++ Main/DotinClass/src/main/java/Section2/MainApp.java
@@ -1,5 +1,15 @@
 package Section2;
 
-public class MainApp {
+import static javax.swing.text.StyleConstants.Family;
 
+public class MainApp {
+    public static void main(String[] args) {
+        String name="Amirhossein";
+        String family="Mohammadi";
+        int age=34;
+        String gender="Male";
+        System.out.println("My Name "+name+"\n"+"Family :"+family+"\n"+
+                "Age:"+age+"\n"+
+                "Gender :"+gender);
+    }
 }
