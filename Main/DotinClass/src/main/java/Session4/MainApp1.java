32262cd239cccdf423d9edf3f927a65f1bc1a149
Amirhossein <amir.kingstars@gmail.com>
2024-06-18 14:59:10 +0330

    commit

---
+++ Main/DotinClass/src/main/java/Session4/MainApp1.java
@@ -0,0 +1,13 @@
+package Session4;
+
+public class MainApp1 {
+    public static void main(String[] args) {
+        int number=8;
+        if (number%2==0){
+            System.out.println("Number is even");
+        }
+        else if (number%2==1){
+            System.out.println("Number is odd");
+        }
+    }
+}
---
+++ Main/DotinClass/src/main/java/Session5/MainApp1.java
@@ -0,0 +1,10 @@
+package Session5;
+
+public class MainApp1 {
+    public static void main(String[] args) {
+        String names[]={"Ali","Reza","Mohammad","Masoud"};
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
