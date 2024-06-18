cb7b3979f3cc79819a5417dd30467f0678c35271
Amirhossein <amir.kingstars@gmail.com>
2024-06-18 16:09:16 +0330

    commit

---
+++ Main/DotinClass/src/main/java/Session5/MainApp3.java
@@ -0,0 +1,17 @@
+package Session5;
+
+public class MainApp3 {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4};
+        System.out.println(avg(numbers));
+
+    }
+    public static int avg(int[] numbers) {
+        int sum = 0;
+        for (int i=0; i<numbers.length; i++) {
+            sum+=numbers[i];
+        }
+        int result=sum/numbers.length;
+        return result;
+    }
+}
