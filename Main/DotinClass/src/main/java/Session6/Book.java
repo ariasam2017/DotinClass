f3551efda858322f8641671caae70c8bdc93e770
Amirhossein <amir.kingstars@gmail.com>
2024-07-09 08:41:35 +0330

    commit

---
+++ Main/DotinClass/src/main/java/Session6/Book.java
@@ -0,0 +1,21 @@
+package Session6;
+
+public class Book {
+    private String title;
+    private String author;
+    private String press;
+    private String releaseDate;
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public void displayInfo(String title, String author, String press, String releaseDate) {
+        System.out.println("Title: " + title+"\nAuthor: " + author+"\nPress: " + press+"\nRelease Date: " + releaseDate);
+    }
+
+
+}
---
+++ Main/DotinClass/src/main/java/Session6/MainApp.java
@@ -0,0 +1,8 @@
+package Session6;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Book book = new Book();
+        book.displayInfo("Khodshenasi","Amir","13212","1403");
+    }
+}
